b1c1ae2736a3c21713c3501bb04cdd35ef65be9c
Ewunet Kassa <ewunetk@yahoo.com>
2019-05-06 15:46:48 -0400

    commiting friend-f

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Friend-Finder-
+
+Friend-Finder- application, A tinder-like dating app that will match the user based on the completion of a quiz and determine compatability based on the differences between answers.
+
+This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match. This app was designed and coded by Ewunet Kassa.
+
+Requirements
+Modularity in the form of separate files for server logic, storing of friends, views, and routing 10-question survey to assess uniqueness of users
+Use express, body-parser, and path npm packages in the server.js file
+Separate JavaScript files for routing (htmlRoutes.js and apiRoutes.js)
+Appropriate GET and POST routes for serving HTML pages and API 
+Separate file for storing friends (friends.js)
+Calculate best match for user once survey is completed and return that match to the user.
+
+Technologies Used
+JavaScript
+jQuery
+node.js
+Express.js
+HTML
+Bootstrap
+
+Packages Used
+body-parser, express
+
+Installing
+Npm install in terminal
+
+npm install
+Type localhost:3000.
+
+Built With
+node.js - Node.
+express - Express.
+
+Author
+Ewunet Kassa
---
+++ app/data/friendData.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Ewunet Kassa",
+      photo: "https://www.google.com/search?q=halle+berry&rlz=1C5CHFA_enUS818US818&tbm=isch&source=iu&ictx=1&fir=8CrvJySCzrn0NM%253A%252CQNyXBHpGvsMSXM%252C%252Fm%252F03knl&vet=1&usg=AI4_-kSBPzFRow5qdt8v6r-yN-nCeGZ-hw&sa=X&ved=2ahUKEwi0neHp0YfiAhWOjlkKHWf7COsQ_B0wLHoECAkQBg#imgrc=8CrvJySCzrn0NM:",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Timothy Marquette",
+      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Matthew Hoffmann",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Mandy Kaine",
+      photo: "https://www.google.com/search?q=halle+berry&rlz=1C5CHFA_enUS818US818&tbm=isch&source=iu&ictx=1&fir=8CrvJySCzrn0NM%253A%252CQNyXBHpGvsMSXM%252C%252Fm%252F03knl&vet=1&usg=AI4_-kSBPzFRow5qdt8v6r-yN-nCeGZ-hw&sa=X&ved=2ahUKEwi0neHp0YfiAhWOjlkKHWf7COsQ_B0wLHoECAkQBg#imgrc=8CrvJySCzrn0NM:",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Alyssa Bonsa",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Ab Smith",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  module.exports = friends;
+  
